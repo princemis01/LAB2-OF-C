d2d8a8202492f8ada7be74c032ce37de0e25e5f1
Prince Mishra <157045372+princemis01@users.noreply.github.com>
2024-09-26 13:02:51 -0400

    LAB 2 IN C

---
+++ lab2.c
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+
+int main() {  
+    
+    int length = 4;  
+    int width = 6;  
+    int area;  
+    
+    area = length * width;  
+    
+    printf("The area of the rectangle is: %d\n", area);  
+    
+    return 0;  
+}
---
+++ number_output.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+  int a = 5;
+  float b = 9.3;
+  
+  printf("%d", a);
+}
